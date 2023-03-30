12368ea102f1b465c735e6d83eabf06fe286cddf
taeheev <121778502+taeheev@users.noreply.github.com>
2023-03-30 16:44:53 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int number = 1;
+        
+        for (int i = 0; i<answer.length; i++){
+        answer[i] = number;
+            number+=2;
+        }   
+        return answer;
+    }
+}
