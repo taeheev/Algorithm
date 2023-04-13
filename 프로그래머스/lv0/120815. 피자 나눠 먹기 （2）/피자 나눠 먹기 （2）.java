5d1a4a782cae384a68d391ab53458112044ce6eb
taeheev <121778502+taeheev@users.noreply.github.com>
2023-04-13 21:03:08 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n) {
         int answer = 1;
-        while(true){
-            if (6 * answer % n == 0) break;
-                answer++;
-        }
+       while(6 * answer % n != 0){
+           answer++;
+       }
         
         return answer;
     }
