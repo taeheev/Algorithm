62c8d72622f22122ea69197ba16f6bacaa2f56ae
taeheev <121778502+taeheev@users.noreply.github.com>
2023-04-14 17:01:54 +0900

    [level 0] Title: 약수 구하기, Time: 0.38 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i++){
+            if(n % i == 0) 
+                cnt++;
+     }    
+        
+    int[] answer = new int[cnt];
+        for (int i=1, j=0; i <= n; i++){
+            if(n % i == 0) {
+            answer[j] = i;
+            j++;
+        }
+    }      
+        Arrays.sort(answer);
+        return answer;
+    }
+}
