254ce0db19f453f502b36f1d68352dd2284fc33b
taeheev <121778502+taeheev@users.noreply.github.com>
2023-06-06 21:00:16 +0900

    [unrated] Title: 문자열 섞기, Time: 12.09 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        String[] str11 = str1.split("");
-        String[] str22 = str2.split("");
-        for(int i = 0; i < str11.length; i++){
-            answer += str11[i] + str22[i];
+        String[] str1arr = str1.split("");
+        String[] str2arr = str2.split("");
+        for(int i = 0; i < str1arr.length; i++){
+            answer += str1arr[i] + str2arr[i];
         }
         return answer;
     }
